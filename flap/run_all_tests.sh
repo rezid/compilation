e4099d0cadb1618d161464d9ee4028fe2eb509bc
ZIDANE Abderrazak <zidane.abderrazak@gmail.com>
2016-11-14 18:48:47 -0800

    add a md5 check to test if a new production rule changed the output of valide file compilation

--- flap/run_all_tests.sh
+++ flap/run_all_tests.sh
@@ -2,17 +2,34 @@ MAKE_OUTPUT="$(make)"
 
 if [ "$?" -eq "0" ]
 then
-    echo " [make]           : success"
+    echo " [make] : success"
 else
     echo "${MAKE_OUTPUT}"
     exit 0
 fi
 
+MD5="$(md5sum ./battery_testing/valide1.target)"
 TEST_OUTPUT="$(./flap --source hopix -V true ./battery_testing/valide1.target)"
 
 if [ "$?" -eq "0" ]
 then
-    echo " [valide1.target] : success (check valide1.hopix)"
+    if [ "${MD5}" == "77ff9bcd9c0c4eac6ae28c1be110cb25  ./battery_testing/valide1.target" ]
+    then
+        echo " [valide1.target] : success (md5 valide) "
+    else
+	echo " [valide1.target] : Error (md5 not valide) "
+    fi
+else
+    echo "${TEST_OUTPUT}"
+    exit 0
+fi
+
+
+TEST_OUTPUT="$(./flap --source hopix -V true ./battery_testing/valide2.target)"
+
+if [ "$?" -eq "0" ]
+then
+    echo " [valide2.target] : success"
 else
     echo "${TEST_OUTPUT}"
     exit 0
